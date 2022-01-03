651933a37f33d58127d5ff3d4425abff4c1f895c
maxwelljkahn <maxwelljkahn@gmail.com>
2022-01-03 14:43:53 -0800

    continue setting up react app awaiting react-router-dom setup

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import React, { useState } from "react";
+import NewTeamPage from "../NewTeamPage/NewTeamPage";
+import AuthPage from "../AuthPage/AuthPage";
 
 function App() {
   const [user, setUser] = useState(null);
-  return <main className="App">App</main>;
+  return <main className="App">{user ? <NewTeamPage /> : <AuthPage />}</main>;
 }
 
 export default App;
