8cac793ed4468e741f8cee7f3231c752c4cedd31
maxwelljkahn <maxwelljkahn@gmail.com>
2022-01-04 14:13:44 -0800

    import Route, Routes - replaces switch, and Navigate - replaces Redirect from react router dom

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,11 +1,29 @@
 import "./App.css";
 import React, { useState } from "react";
-import NewTeamPage from "../NewTeamPage/NewTeamPage";
+import { Route, Routes, Navigate } from "react-router-dom";
 import AuthPage from "../AuthPage/AuthPage";
+import NewTeamPage from "../NewTeamPage/NewTeamPage";
+import TeamHistoryPage from "../TeamHistoryPage/TeamHistoryPage";
 
 function App() {
-  const [user, setUser] = useState(null);
-  return <main className="App">{user ? <NewTeamPage /> : <AuthPage />}</main>;
+  const [user, setUser] = useState({});
+  return (
+    <main className="App">
+      {user ? (
+        <Routes>
+          <Route path="/teams/new">
+            <NewTeamPage />
+          </Route>
+          <Route path="/teams">
+            <TeamHistoryPage />
+          </Route>
+          <Navigate to="/teams" />
+        </Routes>
+      ) : (
+        <AuthPage />
+      )}
+    </main>
+  );
 }
 
 export default App;
