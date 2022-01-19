d05f2dd5ba82e18e7bdc60f8b567ca68cfb715f3
maxwelljkahn <maxwelljkahn@gmail.com>
2022-01-19 15:43:13 -0800

    created JWT based authentication

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,19 @@
+const User = require("../../models/user");
+
 module.exports = {
   create,
 };
 
-function create(req, res) {
-  res.json({
-    user: {
-      name: req.body.name,
-      email: req.body.email,
-    },
-  });
+async function create(req, res) {
+  try {
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    res.json(token);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+function createJWT(user) {
+  return jwt.sign({ user }, process.env.SECRET, { expiresIn: "24h" });
 }
